3dd7e0bd4f131e7a8c99cdd2988dde343e29320a
Antonijejo <Perajovanovic232@gmail.com>
2025-08-19 16:21:05 +0200

    Dodata logika za login, logout i register

---
+++ back/app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+ 
+class AuthController extends Controller
+{
+    public function register(Request $reqest)
+    {
+        $validator = Validator::make($reqest->all(),[
+            'ime_prezime' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+           
+        ]);
+ 
+        if($validator->fails()){
+            return response()->json(['success'=> false, 'data'=> $validator->errors()]);
+        }
+ 
+      
+        $user = User::create([
+            'ime_prezime'=> $reqest->ime_prezime,
+            'email'=> $reqest->email,
+            'password'=> Hash::make($reqest->password),
+            'uloga'=> "korisnik"
+        ]);
+ 
+        $token = $user->createToken('auth_token')->plainTextToken;
+ 
+        return response()->json(['success'=>true,'data'=> $user, 'access_token'=> $token, 'token_type'=> 'Bearer']);
+    }
+ 
+    public function login(Request $request)
+    {
+
+        \Log::info('Login attempt: ', ['email' => $request->email, 'password' => $request->password]);
+      
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json(['success'=> false]);
+        }
+ 
+        $user = User::where('email', $request['email'])->firstOrFail();
+ 
+        $token = $user->createToken('auth_token')->plainTextToken;
+ 
+        return response()->json(['success'=>true,'data'=> $user, 'access_token'=> $token, 'token_type'=> 'Bearer','role'=>$user->uloga]);
+    }
+ 
+    public function logout(Request $request)
+    {
+       $request->user()->tokens()->delete();
+       return response()->json(['message'=> 'Successfully logged out!']);
+    }
+}
